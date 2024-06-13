d61cf4f2fb554460394335dcc1b426a4549c6b80
gangu <gangu@Abhishek>
2024-06-13 09:57:04 +0530

    onPollScmTest is added

---
+++ src/test/java/jenkins/OnPollScmTest.java
@@ -0,0 +1,16 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class OnPollScmTest 
+{
+
+		@Test
+		public void demo()
+		{
+			Reporter.log("OnPollScmTest is executed",true);
+		}
+		
+
+}
